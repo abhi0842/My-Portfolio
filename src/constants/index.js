4ca7db1c8c7d4f753afcefbd6b50298bbfc206f3
abhi0842 <chaudharyabhinav0842@gmail.com>
2025-11-01 12:37:59 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,64 @@
+// import project1 from "../assets/projects/project-1.jpg";
+// import project2 from "../assets/projects/project-2.jpg";
+// import project3 from "../assets/projects/project-3.jpg";
+// import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = ``;
+
+export const ABOUT_TEXT = `Hi, I'm Abhinav, a passionate and detail-oriented Software Engineer with hands-on experience in full-stack development, cloud deployment, and scalable system design. Skilled in Java, Python, React, Node.js, and Spring Boot, with strong expertise in data structures and algorithms. Experienced in developing real-time platforms, interactive web applications, and backend systems using AWS, Docker, and MongoDB. Eager to contribute to impactful projects that combine technical innovation with user-focused design.`;
+
+export const PROJECTS = [
+  {
+    title: "Fitness Tracking Platform",
+    description:
+      "A full-stack MERN application that allows users to track workouts, monitor calorie intake, and view real-time statistics. The platform features secure user authentication, data visualization using Chart.js, and a responsive dashboard. It is deployed on Netlify and Render for seamless accessibility.",
+    technologies: ["MongoDB", "Express.js", "React.js", "Node.js", "Chart.js", "JWT", "Docker"],
+    liveLink: "https://fitnesstrackerabhinav.netlify.app/",
+    githubLink: "https://github.com/abhi0842/Fitness-Tracker/tree/main",
+  },
+  {
+    title: "Grid Pathfinder Visualizer",
+    description: "The Pathfinder Visualizer is an interactive web application that allows users to visualize various graph traversal algorithms such as Dijkstra, DFS, BFS, and A* for finding the shortest path. Users can customize the grid with blocked cells, set start and end points, and observe the algorithm’s progress in real-time. This project was built using ReactJS and JavaScript to create an engaging and intuitive experience for users learning about algorithms.",
+    technologies: ["JavaScript", "React.js"],
+    liveLink: "https://pathfinder-visualizer-sigma.vercel.app/",
+    githubLink: "https://github.com/abhi0842/Pathfinder-Visualizer",
+  },
+  {
+    title: "Travel-Mates",
+    description:
+      "A global traveler connection platform designed to connect travelers around the world. Built with the MERN stack, it features user authentication, profile management, and real-time networking capabilities. Dockerized for deployment and optimized for high performance.",
+    technologies: ["MongoDB", "Express.js", "React.js", "Node.js", "Docker", "JWT"],
+    liveLink: "https://github.com/abhi0842/Travel-Mates",
+    githubLink: "https://github.com/abhi0842/Travel-Mates",
+  },
+  {
+  title: "Object Detection App",
+  description:
+    "An Android application that uses TensorFlow Lite and ML Kit for real-time object detection with over 90% accuracy. The app identifies multiple objects through a live camera feed and processes data efficiently using Python-based preprocessing techniques. Integrated database and API communication improved data management and reduced response time by 30%.",
+  technologies: ["Kotlin", "TensorFlow Lite", "ML Kit", "Python", "Android Studio"],
+  githubLink: "https://github.com/abhi0842/Object-Detection-App",
+},
+  {
+  title: "Meme App",
+  description:
+    "An Android application that fetches and displays random memes from online APIs, allowing users to view, refresh, and share memes seamlessly. The app leverages RESTful APIs for fetching data and Glide for efficient image loading. Designed with a clean and responsive UI to deliver a smooth user experience.",
+  technologies: ["Kotlin", "Android Studio", "RESTful APIs", "Glide"],
+  githubLink: "https://github.com/abhi0842/Meme-App",
+},
+
+ {
+  title: "To-Do App",
+  description:
+    "A task management Android application developed in Kotlin, enabling users to add, edit, and delete daily tasks with ease. Integrated Room Database for persistent data storage and implemented RecyclerView for dynamic task display. Focused on creating an intuitive and responsive user interface for effective task tracking.",
+  technologies: ["Kotlin", "Android Studio", "Room Database", "RecyclerView"],
+  githubLink: "https://github.com/abhi0842/Task-Trove",
+}
+
+];
+
+
+export const CONTACT = {
+  address: "Roorkee, Uttarakhand India ",
+  phoneNo: "+919528085292",
+  email: "007abhinavchaudhary@gmail.com",
+};
