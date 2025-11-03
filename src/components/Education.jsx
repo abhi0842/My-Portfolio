19a4866be66255dce01a6228087e8a56a7ecde1f
abhi0842 <chaudharyabhinav0842@gmail.com>
2025-11-03 16:54:03 +0530

    education file added

---
+++ src/components/Education.jsx
@@ -0,0 +1,60 @@
+import React, { memo } from 'react';
+import { GraduationCap } from 'lucide-react'; 
+import { motion } from 'framer-motion'; 
+
+const EducationItem = memo(({ edu }) => (
+  <motion.div
+    initial={{ opacity: 0, y: 20 }} 
+    whileInView={{ opacity: 1, y: 0 }} 
+    transition={{
+      opacity: { duration: 0.8, ease: "easeInOut" }, 
+      y: { duration: 0.8, ease: "easeInOut" },
+    }}
+    viewport={{ once: true }}
+    className="p-8 mb-12 border border-neutral-800 rounded-3xl shadow-xl bg-gradient-to-r from-purple-800 via-indigo-900 to-blue-900 transform transition duration-200 bg-opacity-80"
+  >
+    <div className="flex items-center mb-2">
+      <GraduationCap className="text-white w-10 h-8 mr-4" />
+      <h3 className="text-2xl font-semibold text-white">{edu.degree}</h3>
+    </div>
+    <p className="text-lg text-gray-300">{edu.institution}</p>
+    <p className="text-lg text-gray-300">{edu.duration}</p>
+    <p className="text-lg text-gray-300">{edu.CGPA}</p>
+  </motion.div>
+));
+
+const Education = () => {
+  const educationData = [
+    {
+      id: 1, 
+      degree: "Bachelor of Technologyy (B.Tech) in Computer Science and Engineering",
+      institution: "Sir Chhotu Ram Institute of Engineering and Technology (SCRIET), Meerut",
+      duration: "2020 - 2024",
+      CGPA: "CGPA: 8.09/10",
+    },
+    
+  ];
+
+  return (
+    <motion.div
+      id="educations"
+      className="relative z-10 border border-neutral-800 rounded-3xl shadow-xl m-2 pt-10 lg:mb-16 px-4 bg-transparent"
+      initial={{ opacity: 0, y: -20 }} 
+      whileInView={{ opacity: 1, y: 0 }} 
+      transition={{ duration: 0.8, ease: "easeInOut" }} 
+      viewport={{ once: true }}
+    >
+      <h2 className="text-center text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 tracking-wide animate-pulse">
+        Education
+      </h2>
+
+      <div className="space-y-8 m-8">
+        {educationData.map((edu) => (
+          <EducationItem key={edu.id} edu={edu} />
+        ))}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Education;
